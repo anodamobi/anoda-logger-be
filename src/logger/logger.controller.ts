f1e436722e77208c5053355cbcb3434f3285782b
Artem Abdullin <artem.abdullin@anoda.mobi>
2022-04-12 17:49:49 +0300

    Added AuthGuard to controllers

--- src/logger/logger.controller.ts
+++ src/logger/logger.controller.ts
@@ -1,17 +1,20 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
 import { LoggerService } from './logger.service';
 import { CreateLoggerDto } from './dto/create-logger.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('logger')
 export class LoggerController {
   constructor(private readonly loggerService: LoggerService) {}
 
   @Post()
+  @UseGuards(AuthGuard('api-key'))
   create(@Body() createLoggerDto: CreateLoggerDto) {
     return this.loggerService.create(createLoggerDto);
   }
 
   @Get()
+  @UseGuards(AuthGuard('api-key'))
   findAll() {
     return this.loggerService.findAll();
   }

ad4c5d6480c12512efcebb5da737158588de92f4
Pavel Stakhieiev <pavelstaheev1@gmail.com>
2022-04-12 15:34:11 +0300

    Authorization added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerModule } from './logger/logger.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from "./auth/auth.module";
 
 @Module({
-  imports: [LoggerModule, MongooseModule.forRoot('mongodb://localhost/logger')],
+  imports: [LoggerModule, AuthModule, MongooseModule.forRoot('mongodb://localhost/logger')],
   controllers: [AppController],
   providers: [AppService],
 })
